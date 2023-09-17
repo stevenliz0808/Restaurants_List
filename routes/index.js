248d7803fe2c2ccbd8ea29a7cace8603d2e3b665
tw3638993 <tw3638993@gmail.com>
2023-09-17 10:45:37 +0800

    add hashing password

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 
 const db = require("../models");
 const User = db.User;
+
+const restaurants = require("./restaurants");
+const authHandler = require("../midlewares/auth-handler");
 
 passport.use(
   new LocalStrategy((username, password, done) => {
@@ -14,10 +18,15 @@ passport.use(
       raw: true
     })
       .then((user) => {
-        if (!user || user.password !== password) {
-          return done(null, false, { message: "帳號或密碼錯誤" });
+        if (!user) {
+          return done(null, false, { message: "帳號不存在" });
         }
-        return done(null, user);
+        return bcrypt.compare(password, user.password).then((isMatch) => {
+          if (!isMatch) {
+            return done(null, false, { message: "密碼錯誤" });
+          }
+          return done(null, user);
+        });
       })
       .catch((error) => {
         error.message = "登入失敗";
@@ -37,8 +46,7 @@ passport.deserializeUser((id, done) => {
   });
 });
 
-const restaurants = require("./restaurants");
-const authHandler = require("../midlewares/auth-handler");
+
 router.use("/restaurants", authHandler, restaurants);
 
 router.get("/", (req, res) => {
@@ -69,23 +77,26 @@ router.post("/register", (req, res, next) => {
     return res.redirect("back");
   }
 
-  return User.findOrCreate({
-    where: { name },
-    defaults: { name, password },
-  })
-    .then(([user, created]) => {
-      if (created) {
-        req.flash("success", "註冊成功，請重新登錄!");
-        res.redirect("/login");
-      } else {
-        req.flash("error", "此用戶已存在");
-        res.redirect("back");
-      }
+  return bcrypt.hash(password, 10)
+    .then((hash) => {
+      return User.findOrCreate({
+        where: { name },
+        defaults: { name, password: hash },
+      })
+        .then(([user, created]) => {
+          if (created) {
+            req.flash("success", "註冊成功，請重新登錄!");
+            res.redirect("/login");
+          } else {
+            req.flash("error", "此用戶已存在");
+            res.redirect("back");
+          }
+        })
+        .catch((error) => {
+          error.message = "註冊失敗";
+          next(error);
+        });
     })
-    .catch((error) => {
-      error.message = "註冊失敗";
-      next(error);
-    });
 });
 
 router.post("/logout", (req, res, next) => {
